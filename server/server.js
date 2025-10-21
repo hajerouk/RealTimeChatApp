5314440098f4e5e237f26e1c76ce7c79272cd124
Hajer OUKHAY <oukhaj@gmail.com>
2025-10-21 12:18:01 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -88,7 +88,7 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) =>
+  app.get("/*", (req, res) =>
     res.sendFile(path.join(__dirname, "../client/build", "index.html"))
   );
 }
